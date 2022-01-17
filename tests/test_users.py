6a0529848b2d292ab028e8b07b8ab1acfe335c60
giangnguyen1992 <tgn1992@hotmail.de>
2022-01-17 22:49:20 +0100

    added database to testing

--- tests/test_users.py
+++ tests/test_users.py
@@ -1,10 +1,17 @@
-from fastapi.testclient import TestClient
-from app.main import app
-
-client = TestClient(app)
+from app import schema
+from .database import client, session
 
 
-def test_root():
+def test_root(client):
     res = client.get("/")
     assert res.json().get("Welcome") == "to root!!!!"
     assert res.status_code == 200
+
+
+def test_create_user(client):
+    res = client.post(
+        "/users/", json={"email": "gggggggiang@web.de", "password": "123"}
+    )
+    new_user = schema.UserResponse(**res.json())
+    assert new_user.email == "gggggggiang@web.de"
+    assert res.status_code == 201
