5e4f8ca951ada1f0f7afc5ad4b2ed07bddfa3f18
giangnguyen1992 <tgn1992@hotmail.de>
2022-01-18 00:47:30 +0100

    finished user test

--- tests/test_users.py
+++ tests/test_users.py
@@ -1,3 +1,5 @@
+from fastapi import status
+import pytest
 from app import schema
 from jose import jwt
 from app.config import settings
@@ -25,3 +27,19 @@ def test_login_user(client, test_user):
     assert id == test_user["id"]
     assert login_res.token_type == "bearer"
     assert res.status_code == 200
+
+
+@pytest.mark.parametrize(
+    "email, password, status_code",
+    [
+        ("wrongemail@gmail.com", "password123", 403),
+        ("sanjeev@gmail.com", "wrongpassword", 403),
+        ("wrongemail@gmail.com", "wrongpassword", 403),
+        (None, "password123", 422),
+        ("sanjeev@gmail.com", None, 422),
+    ],
+)
+def test_incorrect_login(test_user, client, email, password, status_code):
+    res = client.post("/login/", data={"username": email, "password": password})
+
+    assert res.status_code == status_code
